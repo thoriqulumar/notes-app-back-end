9bfc7ce0b37a9a76607e456f6d742ae95efe2587
thoriqulumar <bang.thor.tu@gmail.com>
2022-07-20 10:00:35 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addHandler,
+  getAllNoteHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("./handler");
+
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
